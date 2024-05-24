86b67c4f665494e1342e294cd208a4151ff2eda2
surespraja <sureshprajapati076@gmail.com>
2024-05-24 16:02:19 -0700

    test

--- src/main/java/dev/danvega/hj17/SimpleHandler.java
+++ src/main/java/dev/danvega/hj17/SimpleHandler.java
@@ -4,12 +4,12 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class SimpleHandler implements RequestHandler<String,String> {
+public class SimpleHandler implements RequestHandler<Student,String> {
 
-    public String handleRequest(String input, Context context) {
-        LambdaLogger logger = context.getLogger();
-        logger.log("JDK Version: " + System.getProperty("java.version"));
-        return input.toUpperCase();
+    public String handleRequest(Student input, Context context) {
+            LambdaLogger logger = context.getLogger();
+            return input.name().toUpperCase();
+
     }
 
 }
---
+++ src/main/java/dev/danvega/hj17/Student.java
@@ -0,0 +1,4 @@
+package dev.danvega.hj17;
+
+public record Student(String name) {
+}
